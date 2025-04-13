c15f9980a95eeaa77e6d150d386683f960c9c81a
Jasonmmill <jasonmatthewmill@gmail.com>
2025-04-13 00:01:23 -0400

    Setup root component with state and props structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React, {useState} from 'react'
 import Gallery from './components/Gallery'
 
-function App() {
-  const [cards, setCards] = useState([]);
+function App() { // main app component
+  const [cards, setCards] = useState([]); // state to hold the card data
 
-  const removeCard = (id) => {
+  const removeCard = (id) => { // function to remove a card by id
     setCards((prevCards) => prevCards.filter((card) => card.id !== id));
   }
 
-  return (
+  return ( // rendering the main app 
     <main>
       <h1 className='web-head'> Explore our Tours!</h1>
       <Gallery cards={cards} setCards={setCards} onRemove={removeCard} />
@@ -17,3 +17,5 @@ function App() {
 }
 
 export default App
+
+const jason = "jason"
